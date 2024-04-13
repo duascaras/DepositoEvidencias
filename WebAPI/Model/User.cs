62ba5c754a08ffc052d726a45d27f02e43381439
TheMBS7 <matheus.branco1@hotmail.com>
2024-04-13 18:17:49 -0300

    Gerando Token

---
+++ WebAPI/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UsarName { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; }
+    }
+}
---
+++ WebAPI/Model/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Model
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public required string UsarName { get; set; } = string.Empty;
+        public required string Password { get; set; } = string.Empty;
+    }
+}
